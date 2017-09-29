0fdf60fe5c205ab0ba2a7db222185e7c921505eb
thehobbit85 <thehobbit85@gmail.com>
2017-09-29 03:16:08 -0700

    add more options to electrumx config

--- electrumx.sh
+++ electrumx.sh
@@ -2,8 +2,45 @@
 
 export DB_DIRECTORY=/datadrive/electrumx
 export DAEMON_URL=http://randomuser:ai4ufhwuyr93984t@localhost:8332/
-export TCP_PORT=50001
 export BANNER_FILE=/datadrive/banner
 export COIN=BitcoinSegwit
 export NET=mainnet
-export HOST=
+export ALLOW_ROOT=true
+export HOST=0.0.0.0
+export TCP_PORT=50001
+# export RPC_PORT=
+# export SSL_CERTFILE=/etc/electrumx/server.crt
+# export SSL_KEYFILE=/etc/electrumx/server.key
+# export SSL_PORT=401
+# export DB_ENGINE=leveldb
+# export CACHE_MB=100MB
+# export REORG_LIMIT=100
+# export MAX_SUBSCRIPTIONS=10000
+# export BANNER_FILE=None
+# export TOR_BANNER_FILE=none           
+# export ANON_LOGS=False
+# export LOG_SESSIONS=3600
+# export PEER_DISCOVERY=True
+# export PEER_ANNOUNCE=True
+# export FORCE_PROXY=False
+# export TOR_PROXY_HOST=localhost
+# export TOR_PROXY_PORT=None
+# export DONATION_ADDRESS=none
+# export MAX_SEND=1000000
+# export MAX_SUBS=250000
+# export MAX_SESSION_SUBS=50000
+# export BANDWIDTH_LIMIT=2000000
+# export SESSION_TIMEOUT=600
+# export ELECTRUMX
+# export USERNAME
+# export RPC_HOST
+# export EVENT_LOOP_POLICY
+# export MAX_SESSIONS
+# export REPORT_HOST
+# export REPORT_TCP_PORT
+# export REPORT_SSL_PORT
+# export REPORT_HOST_TOR
+# export REPORT_TCP_PORT_TOR
+# export REPORT_SSL_PORT_TOR
+# export IRC
+# export IRC_NICK
