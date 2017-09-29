3500a7eaee27ef0fe51fbadecdd8886bc653eb58
thehobbit85 <thehobbit85@gmail.com>
2017-09-29 03:15:28 -0700

    use correct version of python to run electrumx

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ bitcoind -config=/bitcoin.conf -datadir=/datadrive/bitcoind -daemon
 
 # ElectrumX
 source /electrumx.sh
-/electrumx/electrumx_server.py
+python3.6 /electrumx/electrumx_server.py
 
 # (echo '{ "id": 5, "method": "blockchain.estimatefee", "params": [10] }'; sleep 2) | ncat localhost 50001
